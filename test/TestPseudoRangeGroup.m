7d3cbc90f4eb8d8841e0429964d65602d0e08de9
Jason Anderson <jand271@stanford.edu>
2021-02-28 12:57:10 -0800

    added slightly better test data with more satellites and no repotitions

--- test/TestPseudoRangeGroup.m
+++ test/TestPseudoRangeGroup.m
@@ -4,16 +4,16 @@ classdef TestPseudoRangeGroup < matlab.unittest.TestCase
     methods(Test)
         function test_against_hw2(testCase)
             load(fullfile(fileparts(mfilename('fullpath')),'hw2_first_gnss_solution_data.mat'));
-            x = -2.703952629951560e+06;
-            y = -4.263104282040041e+06;
-            z = 3.885071813390337e+06;
-            b = -1.281768152170775e+03;
+            x = -2.704071790285519e+06;
+            y = -4.263427501694307e+06;
+            z = 3.885149158026692e+06;
+            b = -2.169197695509361e+03;
             xr_correct = [x;y;z;b];
             
             prg = PsuedoRangeGroupGNSSLog(gnsslogdata);
             xr_computed = prg.solve_newton_raphson();
             
-            testCase.verifyEqual(xr_computed,xr_correct,'abstol',1e-9);
+            testCase.verifyEqual(xr_computed,xr_correct,'abstol',1e-7);
         end
     end
 end
