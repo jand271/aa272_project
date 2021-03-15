a7723a70f96c4648c82efc35cec01a921e53af5c
Xinwei Liu <liuxw@stanford.edu>
2021-03-14 17:01:06 -0700

    update on ranco

--- demo/ranco.m
+++ demo/ranco.m
@@ -10,8 +10,13 @@ this_file_path = fileparts(mfilename('fullpath'));
 ephemeris = struct();
 ephemeris.gps = loadRINEXNavigation('G',fullfile(this_file_path,'../data/oval30_1'),'BRDC00WRD_R_20210710000_01D_GN.rnx');
 ephemeris.bds = loadRINEXNavigation('C',fullfile(this_file_path,'../data/oval30_1'),'BRDC00WRD_R_20210710000_01D_CN.rnx');
+% ephemeris.gps = loadRINEXNavigation('G',fullfile(this_file_path,'../data/gsb30_1'),'BRDC00WRD_R_20210710000_01D_GN.rnx');
+% ephemeris.bds = loadRINEXNavigation('C',fullfile(this_file_path,'../data/gsb30_1'),'BRDC00WRD_R_20210710000_01D_CN.rnx');
+
+%gnsslogdata = readtable(fullfile(this_file_path,'../data/gsb30_1/raw.csv'));
 
 gnsslogdata = readtable(fullfile(this_file_path,'../data/oval30_1/raw.csv'));
+
 
 gnsslogdata_gps_bds = gnsslogdata(bitor(gnsslogdata.ConstellationType == 1, gnsslogdata.ConstellationType == 5),:);
 gnsslogdata_gps_bds = SatelliteECEFs.append_satellite_positions(ephemeris, gnsslogdata_gps_bds);
@@ -26,7 +31,7 @@ gnsslogdata_gps_bds = gnsslogdata_gps_bds(gnsslogdata_gps_bds.CarrierFrequencyHz
 times = unique(gnsslogdata_gps_bds.TimeNanos);
 n_times = size(times,1);
 
-times = times(50:n_times);
+times = times(55:n_times);
 n_times = size(times,1);
 
 xrs = zeros(4, n_times);
@@ -41,14 +46,64 @@ parfor t = 1:n_times
    [solution_ranco(:,t), satellite_exclusion_solution(:,t)] = ranco_satellite(gnsslogdata_instance);
 end
 
+
 %%
 figure
 hold on
-plot3(solution_ranco(1,:),solution_ranco(2,:),solution_ranco(3,:),'*')
-plot3(satellite_exclusion_solution(1,:),satellite_exclusion_solution(2,:),satellite_exclusion_solution(3,:),'*')
-plot3(solution_raw(1,:),solution_raw(2,:),solution_raw(3,:),'*');
-legend('ranco','satellite exclusion','raw')
-xlabel('x (m)')
-ylabel('y (m)')
-zlabel('z (m)')
-title('comparision solution for ranco vs satellite separation')
+plot(solution_ranco(1,:),solution_ranco(2,:),'c*')
+PlottingUtils.plot_percentile_ellipse(solution_ranco(1:2,:), 0.05, 0.90,'b');
+% plot(satellite_exclusion_solution(1,:),satellite_exclusion_solution(2,:),'r*')
+% PlottingUtils.plot_percentile_ellipse(satellite_exclusion_solution(1:2,:), 0.05, 0.90,'r');
+plot(solution_raw(1,:),solution_raw(2,:),'g*');
+PlottingUtils.plot_percentile_ellipse(solution_raw, 0.05, 0.90,'k');
+legend('ranco','ranco 90%','raw','raw 90%','location','best')
+xlabel('X (m)')
+ylabel('Y (m)')
+title('Compare ECEF solution of RACO against raw data')
+hold off
+
+
+
+figure
+hold on
+% plot(solution_ranco(1,:),solution_ranco(2,:),'b*')
+% PlottingUtils.plot_percentile_ellipse(solution_ranco(1:2,:), 0.05, 0.90,'b');
+plot(satellite_exclusion_solution(1,:),satellite_exclusion_solution(2,:),'c*')
+PlottingUtils.plot_percentile_ellipse(satellite_exclusion_solution(1:2,:), 0.05, 0.90,'b');
+plot(solution_raw(1,:),solution_raw(2,:),'g*');
+PlottingUtils.plot_percentile_ellipse(solution_raw, 0.05, 0.90,'k');
+legend('satellite exclusion','satellite exclusion 90%','raw','raw 90%','location','best')
+hold off
+xlabel('X (m)')
+ylabel('Y (m)')
+title('Compare ECEF solution of satellite exclusion method against raw data')
+
+% legend('ranco','ranco 90%','satellite exclusion','satellite exclusion 90%','raw','raw 90%','location','best')
+%legend('ranco','satellite exclusion','raw','location','best')
+solution_ranco1 = solution_ranco(1,:);
+solution_ranco1 = solution_ranco1(~isnan(solution_ranco1));
+solution_ranco2 = solution_ranco(2,:);
+solution_ranco2 = solution_ranco2(~isnan(solution_ranco2));
+
+ranco_mean_solution = [mean(solution_ranco1),mean(solution_ranco2)]
+
+solution_raw1 = solution_raw(1,:);
+solution_raw1 = solution_raw1(~isnan(solution_raw1));
+solution_raw2 = solution_raw(2,:);
+solution_raw2 = solution_raw2(~isnan(solution_raw2));
+
+raw_mean_solution = [mean(solution_raw1),mean(solution_raw2)]
+
+% 37.428153513491736, -122.16179918380078 23.4696  gsb
+
+%37.429713, -122.169527 21.336ovel 
+
+Truth = lla2ecef([37.429713 -122.169527 21.336]);
+Truth = Truth(1:2);
+
+ranco_diff = norm(ranco_mean_solution - Truth)
+raw_diff = norm(raw_mean_solution - Truth)
+
+% xlabel('X (m)')
+% ylabel('Y (m)')
+% title('Compare ECEF solution of RANCO against raw data')
--- ranco_satellite.m
+++ ranco_satellite.m
@@ -1,35 +1,49 @@
-function [solution_ranco, satellite_exclusion_solution] = ranco_satellite(gnsslogdata)
+function [solution_ranco, solution_satellite_exclusion] = ranco_satellite(gnsslogdata)
 
 % create PsuedoRangeGroup object
+warning('off')
 prg = PsuedoRangeGroupGNSSLog(gnsslogdata,false);
 
 %% Compute RANCO Errors
 
+% base line paramenter: 
+% n_selection = 8
+% upper = 80
+% lower = 20
+
 % generate the permuation of all combinations of 4
-n_selected = 10;
+n_selected = 8;
 rprgs = RANCOPsuedoRangeGroupSet(prg, n_selected);
 
 % compute the errors of all satellites on each subset of 4
 sat_errors = rprgs.satellite_errors();
 
 % compute the thresholds of the same
-thresh = rprgs.threshold();
+% thresh = rprgs.threshold();
 
 % array with only errors that are outliers
-outliers_indices = abs(sat_errors)>thresh;
+%outliers_indices = abs(sat_errors)>thresh;
+
+upper = prctile(sat_errors(:),80).*ones(size(sat_errors,1),1);
+lower = prctile(sat_errors(:),20).*ones(size(sat_errors,1),1);
+outliers_indices = (sat_errors > upper) | ((sat_errors < lower));
+
 outlier_errors = sat_errors;
 outlier_errors(~outliers_indices) = nan;
+
+% figure
+% hold on
+% plot(sat_errors,'o')
+% plot(upper,'-','LineWidth',2)
+% plot(lower,'-','LineWidth',2)
+% hold off
 
 %% Ranco
 
 Ranco_outlier_frequency = sum(outliers_indices');
 all_ranco_solutions = rprgs.solve_each_newton_raphson();
-all_solution_ranco = all_ranco_solutions(:,Ranco_outlier_frequency == min(Ranco_outlier_frequency));
-% all_x = rmoutliers(all_solution_ranco(1,:));
-% all_y = rmoutliers(all_solution_ranco(2,:));
-% all_z = rmoutliers(all_solution_ranco(3,:));
-% all_b = rmoutliers(all_solution_ranco(4,:));
-solution_ranco = [mean([all_solution_ranco(1,:)]) , mean([all_solution_ranco(2,:)]), mean([all_solution_ranco(3,:)]), mean([all_solution_ranco(4,:)])];
+all_ranco_solutions = all_ranco_solutions(:,Ranco_outlier_frequency == min(Ranco_outlier_frequency));
+solution_ranco = [mean(all_ranco_solutions(1,:)) , mean(all_ranco_solutions(2,:)), mean(all_ranco_solutions(3,:)), mean(all_ranco_solutions(4,:))];
 %% Compute which satellites are most frequently outliers
 satellite_outlier_frequency = sum(outliers_indices);
 
@@ -43,5 +57,5 @@ best_satellite_combo_residual      = satellite_outlier_frequency(sort(index(1:n_
 best_satellite_combo_ind = find(ismember(satellite_outlier_frequency, best_satellite_combo_residual));
 best_satellite_combo = gnsslogdata(best_satellite_combo_ind,:);
 prg_satellite = PsuedoRangeGroupGNSSLog(best_satellite_combo,false);
-satellite_exclusion_solution = prg_satellite.solve_newton_raphson();
+solution_satellite_exclusion = prg_satellite.solve_newton_raphson();
 end
